5df8c45f902f8826ee06115af84497c35383fb72
KilianSen <kilian@senger.group>
2025-12-17 18:54:35 +0100

    Update backend URL in Docker Compose and change default log level to INFO

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ class Config:
     BACKEND_URL = os.getenv("CLOGS_BACKEND_URL", "http://localhost:8000")
     HEARTBEAT_INTERVAL = int(os.getenv("CLOGS_AGENT_HEARTBEAT_INTERVAL", "5"))
     DISCOVERY_INTERVAL = int(os.getenv("CLOGS_AGENT_DISCOVERY_INTERVAL", "1"))
-    LOG_LEVEL = os.getenv("CLOGS_AGENT_LOG_LEVEL", os.getenv("CLOGS_LOG_LEVEL", "DEBUG"))
+    LOG_LEVEL = os.getenv("CLOGS_AGENT_LOG_LEVEL", os.getenv("CLOGS_LOG_LEVEL", "INFO"))
     API_KEY = os.getenv("CLOGS_AGENT_API_KEY", "")
     MONITORING_TAG = os.getenv("CLOGS_AGENTS_MONITORING_TAG", "clogs.monitoring.enabled=true")
 
